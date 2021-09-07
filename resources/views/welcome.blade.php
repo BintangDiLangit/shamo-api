72ee5270c562fe647acf744825dae9b8ac982bac
bintangdilangit <syekhuda463@gmail.com>
2021-09-08 06:47:30 +0700

    edit display v3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -403,16 +403,9 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="ml-4 text-center text-lg text-gray-500">SHAMO API LARAVEL | BINTANGMFHD</div>
+        <div class="ml-4 text-center text-lg text-gray-500">SHAMO API LARAVEL | BINTANGMFHD |
+            Shamo | <a href="{{ asset('shamo-api.postman_collection.json') }}" download>Postman Download</a></div>
     </div>
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="text-center text-sm"><a href="{{ asset('shamo-api.postman_collection.json') }}" download>
-                Postman
-                Shamo | Download</a>
-        </div>
-    </div>
-
 </body>
 
 </html>
